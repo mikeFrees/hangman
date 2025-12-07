28307c53f1594a4b715f07887bdf2b2f2c061436
Frees Mike <mike_frees@outlook.com>
2025-12-07 22:15:41 +0100

    started game logic

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,8 @@
 import './Game.css';
 import Menu from '../components/layout/Menu';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { GameContext } from '../context/GameContext';
+import data from '../assets/data.json';
 
 function Game() {
   const {
@@ -13,14 +14,28 @@ function Game() {
     setAttemptsLeft,
     guessedLetters,
     setGuessedLetters,
+    usedWords,
+    setUsedWords,
+    category,
   } = useContext(GameContext);
 
   const [menuEnabled, setMenuEnabled] = useState(false);
+  const [newGame, setNewGame] = useState(true);
+
+  useEffect(() => {
+    if (newGame) {
+      setAttemptsLeft(10);
+      setGuessedLetters([]);
+      setWord(getWord());
+      setNewGame(false);
+    }
+  }, []);
+
   return (
     <>
       <h2>{status}</h2>
       {menuEnabled ? <Menu disableMenu={disableMenu} /> : ''}
-      <p>I am Game! </p>
+      <p>{word}</p>
       <button onClick={enableMenu}>menu</button>
     </>
   );
@@ -33,6 +48,17 @@ function Game() {
   function disableMenu() {
     setMenuEnabled(false);
     setStatus('playing');
+  }
+
+  function getWord() {
+    const items = data.categories[category];
+    const available = items.filter(item => !usedWords.includes(item.name));
+    if (available.length === 0) return null;
+
+  const randomIndex = Math.floor(Math.random() * available.length);
+  const chosen = available[randomIndex];
+  setUsedWords([...usedWords, chosen.name]);
+  return chosen.name;
   }
 }
 
